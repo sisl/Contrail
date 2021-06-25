2ab3e5b3932e31c527a07998bae51b5d97577839
Dea Dressel <ddressel@stanford.edu>
2021-06-25 14:53:13 -0600

    initial commit

--- scr/draw_trajs/draw_trajs_table.py
+++ scr/draw_trajs/draw_trajs_table.py
@@ -13,6 +13,7 @@ import collections
 
 import json
 
+#testing
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
