8172df4fafa68b42c4cf83844ffeb9a7e4282168
Dea Dressel <ddressel@stanford.edu>
2021-11-28 19:49:13 -0800

    initial commit;

---
+++ contrail/scr/pages/about_page.py
@@ -0,0 +1,31 @@
+
+
+
+import dash
+import dash_bootstrap_components as dbc
+
+from dash import dcc
+from dash import html
+
+import dash_leaflet as dl
+
+# Import Dash Instance #
+from app import app
+
+layout = html.Div([
+    dbc.Container([
+
+        dbc.Row([
+            html.Div(id='mit-license', children=["INCLUDE IMPORTANT LICENSING INFO ABOUT THE PRODUCT HERE\n"]),
+        ],
+        align='left',
+        justify='center',
+        no_gutters=True),
+        dbc.Row([
+            html.Div(id='description', children=["any copyright stuff here\n"]),
+        ],
+        align='left',
+        justify='center',
+        no_gutters=True)
+    ])
+])
